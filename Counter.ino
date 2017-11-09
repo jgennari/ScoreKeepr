71422edb0949c4a3f3201829d1d5540513186351
Joey Gennari <jgennari@gmail.com>
2017-11-09 10:22:19 -0800

    Added source

---
+++ Counter.ino
@@ -0,0 +1,278 @@
+#include <Bounce2.h>
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+#include <SPI.h>
+#include <RH_RF69.h>
+#include <avr/sleep.h>
+#include "PinChangeInterrupt.h"
+#include "LowPower.h"
+//#include <EEPROM.h>
+
+#define RF69_FREQ     915.0
+#define RFM69_CS      10
+#define RFM69_INT     3
+#define RFM69_RST     9
+#define RFM69_EN      A0
+
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
+Adafruit_7segment matrix = Adafruit_7segment();
+
+Bounce P1U_db = Bounce(); 
+Bounce P1D_db = Bounce(); 
+Bounce P2U_db = Bounce(); 
+Bounce P2D_db = Bounce(); 
+const int P1U = 5;
+const int P1D = 6;
+const int P2U = 4;
+const int P2D = A2;
+unsigned long lastChange, lastScoreCheck;
+int P1Score, P2Score, P1UState, P1DState, P2UState, P2DState;
+boolean P1UChanged,P1DChanged,P2UChanged,P2DChanged;
+int shortPress = 1500;
+boolean isserver = true;
+boolean sleeping = false;
+int min_dim = 5;
+int min_off = 10;
+int level_dim = 4;
+int level_bright = 12;
+
+void setup() {
+  matrix.begin(0x70);
+  matrix.setBrightness(level_bright);      
+    
+  pinMode(P1U,INPUT_PULLUP);  
+  P1U_db.attach(P1U);
+  P1U_db.interval(50);  
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(P1U), wake, CHANGE);
+
+  pinMode(P1D,INPUT_PULLUP);
+  P1D_db.attach(P1D);
+  P1D_db.interval(50);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(P1D), wake, CHANGE);
+  
+  pinMode(P2U,INPUT_PULLUP);
+  P2U_db.attach(P2U);
+  P2U_db.interval(50);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(P2U), wake, CHANGE);
+
+  
+  pinMode(P2D,INPUT_PULLUP);
+  P2D_db.attach(P2D);
+  P2D_db.interval(50);
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(P2U), wake, CHANGE);
+  
+  P1Score = 0;
+  P2Score = 0;
+    
+  pinMode(RFM69_RST, OUTPUT);
+  digitalWrite(RFM69_RST, LOW);
+  
+  digitalWrite(RFM69_RST, HIGH);
+  delay(10);
+  digitalWrite(RFM69_RST, LOW);
+  delay(10);
+
+  if (!rf69.init())
+    writeErrorCode(1042);
+  
+  if (!rf69.setFrequency(RF69_FREQ)) 
+    writeErrorCode(1043);
+   
+  rf69.setTxPower(20, true);
+  
+  uint8_t key[] = { 0x29, 0x7F, 0x03, 0x04, 0x05, 0x06, 0x11, 0x08,
+                    0x01, 0x15, 0x03, 0x04, 0x05, 0x06, 0x04, 0x08};
+  rf69.setEncryptionKey(key);
+  
+  
+  matrix.print(10000, DEC);
+  matrix.writeDisplay();
+  delay(500);
+  
+  matrix.clear();
+  matrix.writeDisplay();
+  lastChange = millis();    
+}
+
+void parseScore(char recv[]) {
+  matrix.writeDigitRaw(2, 0x08);  
+  matrix.writeDisplay();
+    
+  char * pch;
+  pch = strtok (recv,":");
+  int p1s = NULL;
+  int p2s = NULL;
+  
+  while (pch != NULL)
+  {
+    if (p1s != NULL)  
+      p2s = (int)pch;
+    else
+      p1s = (int)pch;
+      
+    pch = strtok (NULL, " ,.-");
+  }
+
+  P1Score = p1s;
+  P2Score = p2s;
+  
+  delay(50);
+  matrix.writeDigitRaw(2, 0x00);
+  matrix.writeDisplay();
+}
+
+void sendScore() {
+  matrix.writeDigitRaw(2, 0x04);
+  matrix.writeDisplay();
+  char data[20];
+  strcpy(data, P1Score);
+  strcat(data, ":");
+  strcat(data, P2Score);
+
+  rf69.send(data, sizeof(data));
+  rf69.waitPacketSent(100);
+  delay(50);
+  
+  matrix.writeDigitRaw(2, 0x00);
+  matrix.writeDisplay();
+}
+
+void writeErrorCode(int errorcode) {  
+  matrix.clear();
+  matrix.writeDisplay();
+  matrix.print(errorcode,DEC);
+  matrix.writeDisplay();
+  while(1);
+}
+
+void changeScore(char player, char dir) {
+  switch(player) {
+    case '1':
+      switch(dir) {
+        case 'U':
+          P1Score++;
+          break;
+        case 'D':
+          P1Score--;
+          break;
+      }
+      break;
+    case '2':
+      switch(dir) {
+        case 'U':
+          P2Score++;
+          break;
+        case 'D':
+          P2Score--;
+          break;
+      }
+      break;
+  }
+
+  sendScore();
+}
+
+void loop() {   
+  if (lastChange > 0 && millis() - lastChange > (min_off * 60000)) {
+    matrix.clear();
+    matrix.writeDisplay();     
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    sleeping = true;   
+  } 
+  else if (lastChange > 0 && millis() - lastChange > (min_dim * 60000)) {
+    matrix.setBrightness(level_dim);  
+  } 
+  else {
+    matrix.setBrightness(level_bright);      
+  }
+  
+  P1UChanged = P1U_db.update();
+  P1DChanged = P1D_db.update();
+  P2UChanged = P2U_db.update();
+  P2DChanged = P2D_db.update();
+
+  if (P1UChanged || P1DChanged || P2UChanged || P2DChanged)
+    lastChange = millis();
+
+  int duration = millis()-lastChange;
+  int P1UState = P1U_db.read();
+  int P1DState = P1D_db.read();
+  int P2UState = P2U_db.read();
+  int P2DState = P2D_db.read();
+
+  if (P1U_db.rose() && P1UChanged && duration < shortPress) 
+    changeScore('1', 'U');
+  else if (P1D_db.rose() && P1DChanged && duration < shortPress && P1Score > 0) 
+    changeScore('1', 'D');
+  else if (P2U_db.rose() && P2UChanged && duration < shortPress) 
+    changeScore('2', 'U');
+  else if (P2D_db.rose() && P2DChanged && duration < shortPress && P2Score > 0) 
+    changeScore('2', 'D');
+  else if (P2DState == LOW && duration > shortPress) 
+    resetScores();
+  //else if (P1DState == LOW && duration > shortPress) 
+  //  cycleBrightness();
+
+  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (lastScoreCheck == NULL || millis() - lastScoreCheck > 200) {
+    if (rf69.waitAvailableTimeout(100))  {       
+      if (rf69.recv(buf, &len)) {
+        parseScore((char*)buf);
+      }
+    }   
+    
+    lastScoreCheck = millis();
+  }
+  
+  writeScore(P1Score,1);
+  writeScore(P2Score,4);
+
+  if (!sleeping)
+    matrix.writeDisplay();
+}
+
+void resetScores() {
+  P1Score = 0;
+  P2Score = 0;
+  sendScore();
+  matrix.clear();
+  matrix.writeDisplay();
+  lastChange = millis();
+}
+
+void cycleBrightness() {
+  if (level_bright > 15)
+    level_bright = 1;
+  matrix.setBrightness(level_bright);
+  matrix.clear();
+  matrix.println(level_bright);
+  matrix.writeDisplay();
+  delay(500);
+  matrix.clear();
+  matrix.writeDisplay();
+}
+
+void writeScore(int score, int dpos) {
+  int value = score;
+  int itteration = 0;
+  
+  if (value == 0)
+    matrix.writeDigitNum(dpos-itteration,0,false);
+    
+  while (value > 0) {
+    int digit = value % 10;
+    matrix.writeDigitNum(dpos-itteration,digit,false);
+    value /= 10;
+    itteration++;
+  }
+}
+
+void wake() {
+  sleeping = false;
+  lastChange = millis();
+}
+
+
+
